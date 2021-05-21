394189e24cce3b675e0da6e15bdfa2b35912ac82
Thomas Wang <thomas@teaminkling.com>
2021-05-21 12:44:04 +1000

    Clean up the parser code a little

--- bot/parser.py
+++ bot/parser.py
@@ -10,54 +10,29 @@ from logging import Logger
 from typing import Any, Dict, List, Optional, Pattern, Set, Tuple, Union
 from urllib.parse import quote
 
-# Initialise logging.
+from bot.regexes import (
+    CONTENT_LINK_REGEX,
+    HYPERLINK_REGEX,
+    MEDIUM_PARSING_REGEX,
+    RAW_SOCIAL_PARSING_REGEX,
+    SINGLE_NEWLINE_REGEX,
+    SOCIALS_ITEMIZATION_NATURAL_LANGUAGE_REGEX,
+    SOCIALS_ITEMIZATION_PARENTHESES_REGEX,
+    SOCIALS_ITEMIZATION_WORD_ON_REGEX,
+    SOCIAL_PROVIDER_TO_LINK_REGEX,
+    TITLE_PARSING_REGEX,
+    WEEK_PARSING_REGEX,
+)
+
 
 LOGGER: Logger = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.WARNING)
-log_formatter: logging.Formatter = logging.Formatter(
-    "(%(asctime)s) [%(levelname)s]: %(message)s",
-    "%Y-%m-%d %H:%M:%S",
-)
-
-stream_handler: logging.StreamHandler = logging.StreamHandler()
-
-stream_handler.setLevel(logging.WARNING)
-stream_handler.setFormatter(log_formatter)
-
-logging.root.handlers.clear()
-
-LOGGER.addHandler(stream_handler)
-
 # Constants.
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-WEEK_PARSING_REGEX: Pattern = re.compile(
-    (
-        r"(?P<preamble>[\s\S]*?)(?<!my )(?<!for )(?:week)(?!-)(?:.{0,16}?)(?P<week>[0-9]+|One)"
-        r"(?:.*)(?:[\s])(?P<remainder>[\S\s]+?)(?:(?<!title: )(?=week.{1,16}?[0-9]+)"
-        r"(?!week-)(?!week \d+[ ,])|(?:\Z))"
-    ),
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""
-A complex parsing regex that retrieves, in one content string, matches where each match is the 
-entire body of a weekly submission where there is a group that takes the integer week value and the
-preamble and remainder of the content to be parsed for optional values.
-
-Note that this can't handle one post for multiple submissions if there are attachments.
-
-Also note that this isn't perfect and there will need to be exceptions in many places in the code
-to have this work for every submission.
-"""
 
 with open("in/replacements.json") as replacements_json_file:
     REPLACEMENTS_CONFIG_MAP: dict = json.load(replacements_json_file)
     """
     A map containing replacements and expected missing values for invalid submissions.
-    
-    Note that this is pre-processing. If the parser got something wrong or the formatting just 
-    wasn't very good for the final blog post, I suggest using the `formatting.json` file instead.
     """
 
 with open("in/socials.json") as socials_json_file:
@@ -65,40 +40,6 @@ with open("in/socials.json") as socials_json_file:
     """
     A map containing socials to ignore to help with erroneous social inputs.
     """
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-TITLE_PARSING_REGEX: Pattern = re.compile(
-    r"(?P<preamble>[\s\S]*?)(?:title[:\-* ]+)(?P<title>.*$)(?P<remainder>[\s\S]*)",
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex that retrieves a title if applicable."""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-MEDIUM_PARSING_REGEX: Pattern = re.compile(
-    (
-        r"(?P<preamble>[\s\S]*?)"
-        r"(?<!social )"
-        r"(?:medi(?:(?:um)|a)[,:\-*]*[\s]+)"
-        r"(?P<medium>.*$)"
-        r"(?P<remainder>[\s\S]*)"
-    ),
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex that retrieves a title if applicable."""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-RAW_SOCIAL_PARSING_REGEX: Pattern = re.compile(
-    (
-        r"(?P<preamble>[\s\S]*?)"
-        r"(?P<raw_socials>socia"
-        r"(?:(?:ls)|"
-        r"(?:l media)|"
-        r"(?:sl)|l)?(?! life)[:\-*]*[\s]+)"
-        r"(?P<remainder>[\s\S]*)"
-    ),
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex that splits text into preamble and remainder after the socials marker."""
 
 TEMPLATE_FRAGMENT: str = """
 **Submission Template**
@@ -111,157 +52,6 @@ Social Media:
 ``
 """.strip()
 """Part of the template post which gets ignored."""
-
-
-HYPERLINK_REGEX: Pattern = re.compile(
-    r"https?://[A-Za-z0-9./\-_~!+,*:@?=&$#]*",
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""
-Regex that finds simple hyperlinks.
-
-Notes
------
-This is not meant to be extremely accurate but pick up most links you would find pasted into 
-Discord. There are niche and non-ASCII examples that will break this but we do not consider them.
-"""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-CONTENT_LINK_REGEX: Pattern = re.compile(
-    (
-        r"(?:youtu\.be/\S)|"
-        r"(?:youtube\.com/watch\?v="
-        r"(?!xGP1pUeVJYA)"
-        r"(?!dQw4w9WgXcQ))|"
-        r"(?:soundcloud\.com/\S+/\S)|"
-        r"(?:itch\.io/\S)|"
-        r"(?:docs\.google\.com)|"
-        r"(?:imgur\.com/a/\S)|"
-        r"(?:vimeo\.com)|"
-        r"(?:webtoons\.com)|"
-        r"(?:fliphtml5\.com)"
-    ),
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex used to parse content links, i.e., if they match a hyperlink, it's content."""
-
-SINGLE_NEWLINE_REGEX: Pattern = re.compile(
-    r"(?<=\S)\n(?=[A-Za-z0-9(}\[\]_*])",
-    flags=re.MULTILINE,
-)
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-SOCIAL_PATTERN_PARENTHESES: Pattern[str] = re.compile(
-    r"(?P<replacement>@(?P<username>\S+) +(?:\((?P<platforms>.*?)\)))",
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex used to find social patterns based on parentheses."""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-SOCIAL_PATTERN_ON: Pattern[str] = re.compile(
-    (
-        r"(?P<replacement>"
-        r"(?P<username>\S+) on "
-        r"(?P<platform>"
-        r"(?:(?:tw?itter)|"
-        r"(?:twitch)|"
-        r"(?:insta)|"
-        r"(?:everything)|"
-        r"(?:ig)).*))"
-    ),
-    flags=re.MULTILINE | re.IGNORECASE,
-)
-"""Regex used to find social patterns based on the word "on"."""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-NATURAL_LANGUAGE_SOCIALS: Pattern[str] = re.compile(
-    r"(?: and )|(?: \+ )|\|| |/", flags=re.MULTILINE | re.IGNORECASE
-)
-"""A pattern that allows general processing of a litany of different ways to itemise socials."""
-
-# noinspection RegExpUnnecessaryNonCapturingGroup
-SOCIAL_PATTERNS = [
-    (
-        "Instagram",
-        re.compile(
-            r"(?P<link>(?:(?:Insta(?:gram)?)|(?:IG)[: ,-]+)?"
-            r"(?:https?://)?"
-            r"(?:www\.)?instagram\.com/"
-            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Twitter",
-        re.compile(
-            r"(?P<link>(?:Twitter[: ,-]+)?"
-            r"(?:https?://)?"
-            r"(?:www\.)?(?:mobile\.)?twitter\.com/"
-            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Twitch",
-        re.compile(
-            r"(?P<link>(?:Twitch[: ,-]+)?"
-            r"(?:https?://)?"
-            r"(?:www\.)?twitch\."
-            r"(?:(?:tv)|(?:com))/"
-            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Tumblr",
-        re.compile(
-            r"(?P<link>(?:Tumblr[: ,-]+)?"
-            r"(?:https?://)?"
-            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+)\.tumblr\.com)",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Twitter",
-        re.compile(
-            r"(?P<replacement>twitter[ :=]+@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Instagram",
-        re.compile(
-            r"(?P<replacement>instagram[ :=]+@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Instagram",
-        re.compile(
-            r"(?P<replacement>instagram: +(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-    (
-        "Instagram",
-        re.compile(
-            (
-                r"(?P<replacement>"
-                r"(?:(?i:instagram)|(?:IG)|(?i:Insta))[ =\-:]+@?"
-                r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))"
-            ),
-            flags=re.MULTILINE,
-        ),
-    ),
-    (
-        "Twitch",
-        re.compile(
-            r"(?P<replacement>twitch[ :=]*@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
-            flags=re.MULTILINE | re.IGNORECASE,
-        ),
-    ),
-]
-"""Tuples of a platform to a regex to find that platform as a replacement and username."""
 
 # noinspection SpellCheckingInspection
 PLATFORM_MAP: Dict[str, str] = {
@@ -278,19 +68,6 @@ PLATFORM_MAP: Dict[str, str] = {
     "tumblr": "Tumblr",
 }
 """Mapping of names for platforms to formatted names."""
-
-# noinspection SpellCheckingInspection
-PROBLEMATIC_CONTENT_FRAGMENTS: List[Tuple[str, str]] = [
-    ("So this is it! This is finale of 17 weeks of Designrefesh!", "17"),
-    ("liked the whale from the deep sea week (2)", "17"),
-    ("So this week I am not submitting anything really", "17"),
-    ("So you all remember @papapastry's week 7 song? Well I was do", "17"),
-]
-"""
-Content fragments which will cause the system to simply take the entire post as one week.
-
-The second tuple part is the week to examine the fragmented messages as.
-"""
 
 
 def parse_retrieved() -> None:
@@ -494,7 +271,7 @@ def join_cumulative_messages(retrieved_data: dict) -> List[dict]:
 
 
 def extract_all_content(
-    content: str, author: str, created_at: str, attachments: List[Dict[str, str]]
+    content: str, author: str, created_at: str, attachments: List[Dict[str, str]],
 ) -> List[dict]:
     """
     Extract all information for content, allowing for multiple week submissions in one post.
@@ -536,7 +313,10 @@ def extract_all_content(
     # Handle some extremely headache-inducing submissions that may as well be manual.
 
     if len(matches) > 1:
-        for fragment, replacement_week in PROBLEMATIC_CONTENT_FRAGMENTS:
+        for fragment_to_week in REPLACEMENTS_CONFIG_MAP["manual_week_fragments"]:
+            fragment: str = fragment_to_week["content_fragment"]
+            replacement_week: str = fragment_to_week["week"]
+
             if fragment in content:
                 matches = [("", replacement_week, content)]
 
@@ -764,12 +544,15 @@ def remove_markdown_formatting(preamble: str, remainder: str) -> Tuple[str, str]
     preamble = re.sub(
         r"(?<!\S)_(?P<unformatted>.*)_(?!\S)", r"\g<unformatted>", preamble
     )
+
     remainder = re.sub(
         r"(?<!\S)_(?P<unformatted>.*)_(?!\S)", r"\g<unformatted>", remainder
     )
+
     preamble = re.sub(
         r"(?<!\S)\*(?P<unformatted>.*)\*(?!\S)", r"\g<unformatted>", preamble
     )
+
     remainder = re.sub(
         r"(?<!\S)\*(?P<unformatted>.*)\*(?!\S)", r"\g<unformatted>", remainder
     )
@@ -858,8 +641,8 @@ def parse_socials(text: str) -> Tuple[List[Dict[str, str]], str]:
     # Handle situations where people put socials in brackets, use "X on Y", and have multiple of
     # the same username on different social platforms.
 
-    for match in list(re.findall(SOCIAL_PATTERN_PARENTHESES, text)) + list(
-        re.findall(SOCIAL_PATTERN_ON, text)
+    for match in list(re.findall(SOCIALS_ITEMIZATION_PARENTHESES_REGEX, text)) + list(
+        re.findall(SOCIALS_ITEMIZATION_WORD_ON_REGEX, text)
     ):
         replacement: str = match[0]
         username: str = match[1].replace("@", "").replace("/", "")
@@ -869,7 +652,9 @@ def parse_socials(text: str) -> Tuple[List[Dict[str, str]], str]:
             continue
 
         platform: str
-        for platform in re.split(NATURAL_LANGUAGE_SOCIALS, platforms_text):
+        for platform in re.split(
+            SOCIALS_ITEMIZATION_NATURAL_LANGUAGE_REGEX, platforms_text
+        ):
             # Parse out unnecessary punctuation.
 
             platform = platform.lower().replace("!", "").replace(".", "")
@@ -889,7 +674,7 @@ def parse_socials(text: str) -> Tuple[List[Dict[str, str]], str]:
 
     # Handle remainder cases.
 
-    for name, regex in SOCIAL_PATTERNS:
+    for name, regex in SOCIAL_PROVIDER_TO_LINK_REGEX:
         for social in re.findall(regex, text):
             if social[1]:
                 LOGGER.info(
@@ -1011,4 +796,23 @@ def match_replacement_or_expected_missing(
 
 
 if __name__ == "__main__":
+    # Initialise logger.
+
+    logging.basicConfig(level=logging.WARNING)
+    log_formatter: logging.Formatter = logging.Formatter(
+        "(%(asctime)s) [%(levelname)s]: %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
+
+    stream_handler: logging.StreamHandler = logging.StreamHandler()
+
+    stream_handler.setLevel(logging.WARNING)
+    stream_handler.setFormatter(log_formatter)
+
+    logging.root.handlers.clear()
+
+    LOGGER.addHandler(stream_handler)
+
+    # Run.
+
     parse_retrieved()
---
+++ bot/regexes.py
@@ -0,0 +1,212 @@
+"""A collection of regular expressions used within the processing scripts."""
+
+import re
+from typing import Pattern
+
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+WEEK_PARSING_REGEX: Pattern = re.compile(
+    (
+        r"(?P<preamble>[\s\S]*?)(?<!my )(?<!for )(?:week)(?!-)(?:.{0,16}?)(?P<week>[0-9]+|One)"
+        r"(?:.*)(?:[\s])(?P<remainder>[\S\s]+?)(?:(?<!title: )(?=week.{1,16}?[0-9]+)"
+        r"(?!week-)(?!week \d+[ ,])|(?:\Z))"
+    ),
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""
+A complex parsing regex that retrieves, in one content string, matches where each match is the 
+entire body of a weekly submission where there is a group that takes the integer week value and the
+preamble and remainder of the content to be parsed for optional values.
+
+Note that this can't handle one post for multiple submissions if there are attachments.
+
+Also note that this isn't perfect and there will need to be exceptions in many places in the code
+to have this work for every submission.
+"""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+TITLE_PARSING_REGEX: Pattern = re.compile(
+    r"(?P<preamble>[\s\S]*?)(?:title[:\-* ]+)(?P<title>.*$)(?P<remainder>[\s\S]*)",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex that retrieves a title if applicable."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+MEDIUM_PARSING_REGEX: Pattern = re.compile(
+    (
+        r"(?P<preamble>[\s\S]*?)"
+        r"(?<!social )"
+        r"(?:medi(?:(?:um)|a)[,:\-*]*[\s]+)"
+        r"(?P<medium>.*$)"
+        r"(?P<remainder>[\s\S]*)"
+    ),
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex that retrieves a medium if applicable."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+RAW_SOCIAL_PARSING_REGEX: Pattern = re.compile(
+    (
+        r"(?P<preamble>[\s\S]*?)"
+        r"(?P<raw_socials>socia"
+        r"(?:(?:ls)|"
+        r"(?:l media)|"
+        r"(?:sl)|l)?(?! life)[:\-*]*[\s]+)"
+        r"(?P<remainder>[\s\S]*)"
+    ),
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex that splits text into preamble and remainder after the socials marker."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+HYPERLINK_REGEX: Pattern = re.compile(
+    r"https?://[A-Za-z0-9./\-_~!+,*:@?=&$#]*",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""
+Regex that finds simple hyperlinks.
+
+Notes
+-----
+This is not meant to be extremely accurate but pick up most links you would find pasted into 
+Discord. There are niche and non-ASCII examples that will break this but we do not consider them.
+"""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+CONTENT_LINK_REGEX: Pattern = re.compile(
+    (
+        r"(?:youtu\.be/\S)|"
+        r"(?:youtube\.com/watch\?v="
+        r"(?!xGP1pUeVJYA)"
+        r"(?!dQw4w9WgXcQ))|"
+        r"(?:soundcloud\.com/\S+/\S)|"
+        r"(?:itch\.io/\S)|"
+        r"(?:docs\.google\.com)|"
+        r"(?:imgur\.com/a/\S)|"
+        r"(?:vimeo\.com)|"
+        r"(?:webtoons\.com)|"
+        r"(?:fliphtml5\.com)"
+    ),
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex that recognises if a link is one of the accepted media sites."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+SINGLE_NEWLINE_REGEX: Pattern = re.compile(
+    r"(?<=\S)\n(?=[A-Za-z0-9(}\[\]_*])",
+    flags=re.MULTILINE,
+)
+"""Regex used to recognise when there is exactly one newline in text and matches that newline."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+SOCIALS_ITEMIZATION_PARENTHESES_REGEX: Pattern[str] = re.compile(
+    r"(?P<replacement>@(?P<username>\S+) +(?:\((?P<platforms>.*?)\)))",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex used to find social patterns based on parentheses."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+SOCIALS_ITEMIZATION_WORD_ON_REGEX: Pattern[str] = re.compile(
+    (
+        r"(?P<replacement>"
+        r"(?P<username>\S+) on "
+        r"(?P<platform>"
+        r"(?:(?:tw?itter)|"
+        r"(?:twitch)|"
+        r"(?:insta)|"
+        r"(?:everything)|"
+        r"(?:ig)).*))"
+    ),
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+"""Regex used to find social patterns based on the word "on"."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+SOCIALS_ITEMIZATION_NATURAL_LANGUAGE_REGEX: Pattern[str] = re.compile(
+    r"(?: and )|(?: \+ )|\|| |/", flags=re.MULTILINE | re.IGNORECASE
+)
+"""Regex that allows general processing of a litany of different ways to itemise socials."""
+
+# noinspection RegExpUnnecessaryNonCapturingGroup
+SOCIAL_PROVIDER_TO_LINK_REGEX = [
+    (
+        "Instagram",
+        re.compile(
+            r"(?P<link>(?:(?:Insta(?:gram)?)|(?:IG)[: ,-]+)?"
+            r"(?:https?://)?"
+            r"(?:www\.)?instagram\.com/"
+            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Twitter",
+        re.compile(
+            r"(?P<link>(?:Twitter[: ,-]+)?"
+            r"(?:https?://)?"
+            r"(?:www\.)?(?:mobile\.)?twitter\.com/"
+            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Twitch",
+        re.compile(
+            r"(?P<link>(?:Twitch[: ,-]+)?"
+            r"(?:https?://)?"
+            r"(?:www\.)?twitch\."
+            r"(?:(?:tv)|(?:com))/"
+            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Tumblr",
+        re.compile(
+            r"(?P<link>(?:Tumblr[: ,-]+)?"
+            r"(?:https?://)?"
+            r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+)\.tumblr\.com)",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Twitter",
+        re.compile(
+            r"(?P<replacement>twitter[ :=]+@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Instagram",
+        re.compile(
+            r"(?P<replacement>instagram[ :=]+@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Instagram",
+        re.compile(
+            r"(?P<replacement>instagram: +(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+    (
+        "Instagram",
+        re.compile(
+            (
+                r"(?P<replacement>"
+                r"(?:(?i:instagram)|(?:IG)|(?i:Insta))[ =\-:]+@?"
+                r"(?P<username>[A-Za-z0-9_\-+&%#@^.]+))"
+            ),
+            flags=re.MULTILINE,
+        ),
+    ),
+    (
+        "Twitch",
+        re.compile(
+            r"(?P<replacement>twitch[ :=]*@(?P<username>[A-Za-z0-9_\-+&%#@^.]+))",
+            flags=re.MULTILINE | re.IGNORECASE,
+        ),
+    ),
+]
+"""Tuples of a platform to a regex to find that platform as a replacement and username."""
